59c366a3cd3783cb13bfd2d31d2b703029c35c25
Stephen Lewis <code@stephenlewis.me>
2021-06-09 07:23:01 +0100

    Remove data from view, now that we’re fetching it via Ajax

--- app/Http/Controllers/TurbineController.php
+++ app/Http/Controllers/TurbineController.php
@@ -4,12 +4,10 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-use App\Actions\InspectTurbine;
-
 class TurbineController extends Controller
 {
     public function __invoke()
     {
-        return view('turbine', ['results' => (new InspectTurbine())->inspect()]);
+        return view('turbine');
     }
 }
